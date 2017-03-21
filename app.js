ee909c5a2eaed7026b575effc3a21b8273ad66c3
Brendan Nee <brendan@blinktag.com>
2017-03-21 23:15:41 +0000

    Force SSL code

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const routes = require('./routes');
 
 const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -26,6 +26,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Uncoment below to force SSL
+// if (process.env.NODE_ENV === 'production') {
+//   app.use((req, res, next) => {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(`https://${req.get('Host')}${req.url}`);
+//     }
+//     return next();
+//   });
+// }
 
 app.use('/', routes);
 
