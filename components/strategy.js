06e9f263104b51741fcd78bdf481470c77ae2edd
Brendan Nee <brendan@blinktag.com>
2020-09-28 01:12:15 -0700

    image path

--- components/strategy.js
+++ components/strategy.js
@@ -39,13 +39,13 @@ export default class Strategy extends React.Component {
               onChange={this.toggleSelected}
             />
             <img
-              src={`images/icons/${language}/${strategy.key}.png`}
+              src={`/images/icons/${language}/${strategy.key}.png`}
               alt=""
               className="strategy-image"
               hidden={Boolean(this.props.selected)}
             />
             <img
-              src={`images/icons/${language}/${strategy.key}_inverse.png`}
+              src={`/images/icons/${language}/${strategy.key}_inverse.png`}
               alt=""
               className="strategy-image"
               hidden={Boolean(!this.props.selected)}
