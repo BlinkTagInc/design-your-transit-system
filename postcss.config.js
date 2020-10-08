f8204f2d1dc268aaea045ec13d9d39a9bf5948ab
Brendan Nee <brendan@blinktag.com>
2020-10-07 17:30:50 -0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -7,13 +7,7 @@ const purgecss = [
       './components/**/*.{js,jsx,ts,tsx}'
     ],
     // This is the function used to extract class names from the templates
-    defaultExtractor: content => {
-      // Capture as liberally as possible, including things like `h-(screen-1.5)`
-      const broadMatches = content.match(/[^<>"'`\\s]*[^<>"'`\\s:]/g) || []
-      // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-      const innerMatches = content.match(/[^<>"'`\\s.()]*[^<>"'`\\s.():]/g) || []
-      return broadMatches.concat(innerMatches)
-    }
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
   }
 ]
 
