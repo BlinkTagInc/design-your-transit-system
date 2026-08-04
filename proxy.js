d1cc74c2681eca34bc598c48547ed59ff3087e06
Brendan Nee <brendan@blinktag.com>
2026-08-04 14:28:23 -0700

    middleware -> proxy

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 import { createNextAuthMiddleware } from 'nextjs-basic-auth-middleware'
 
-export const middleware = createNextAuthMiddleware()
+export const proxy = createNextAuthMiddleware()
 
 export const config = {
   matcher: ['/api/export'],
